0d32d7eb831b8a67f59ba007d4dd678a15c101e2
Lucie Kayser-Bril <l.kayserbril@gmail.com>
2022-12-28 23:12:26 -0700

    commit tree f39e3b533290e50c92eeb98bc556972929dc80ee

--- index.js
+++ index.js
@@ -1,6 +1,12 @@
-function isPalindrome(word) {
-  // Write your algorithm here
+function reverseString(word) {
+  return word.split("").reverse().join("");
 }
+  
+function isPalindrome(word) {
+      const reversedWord = reverseString(word);
+      return word === reversedWord;
+}
+
 
 /* 
   Add your pseudocode here
